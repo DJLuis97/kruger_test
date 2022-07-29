68ede87ebd36e3169b88352c5f35a4d40f9a79a8
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-29 13:06:38 -0500

    Ahora se actualiza los datos del empleado

--- kruger_frontend/src/components/FormEmployee.jsx
+++ kruger_frontend/src/components/FormEmployee.jsx
@@ -1,5 +1,133 @@
-const FormEmployee = () => {
-	return "este el es formulario par aempleados";
+import { Box, Button, Checkbox, FormControlLabel, FormGroup, MenuItem, TextField } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+
+const vaccines = [
+	{ id: 1, name: "Sputnik" },
+	{ id: 2, name: "AstraZeneca" },
+	{ id: 3, name: "Pfizer" },
+	{ id: 4, name: "Jhonson&Jhonson" }
+];
+
+const FormEmployee = ({ employee }) => {
+	const [vaccineType, setVaccineType] = useState("AstraZeneca");
+	const [isVaccined, setIsVaccined] = useState(false);
+	const [loadUpdateEmployee, setLoadUpdateEmployee] = useState(false);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setLoadUpdateEmployee(true);
+		const data = new FormData(event.currentTarget);
+		axios
+			.patch(`http://localhost:3001/employees/${employee?.id}`, {
+				bith: data.get("bith"),
+				address: data.get("address"),
+				phone: data.get("phone"),
+				vaccinated: isVaccined,
+				vaccinated_type: isVaccined ? vaccineType : null,
+				vaccinated_date: isVaccined ? data.get("vaccinated_date") : null,
+				vaccinated_dose: isVaccined ? data.get("vaccinated_dose") : null
+			})
+			.then((res) => {
+				if (res.status === 200) {
+					alert("Datos actualizado con éxito");
+				} else {
+					console.log(res);
+				}
+			})
+			.catch((err) => {
+				alert("Algo pasó al actualizar tus datos de empleado");
+			})
+			.finally(() => setLoadUpdateEmployee(false));
+	};
+
+	if (loadUpdateEmployee) return <div>Actualizando...</div>;
+
+	return (
+		<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				id="address"
+				label="Direccion de domicilio"
+				name="address"
+				type="text"
+				autoFocus
+			/>
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				id="bith"
+				label="Fecha de nacimiento"
+				name="bith"
+				type="date"
+			/>
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				id="phone"
+				label="Teléfono móvil"
+				name="phone"
+				type="number"
+			/>
+			<div>
+				<FormControlLabel
+					control={
+						<Checkbox
+							checked={isVaccined}
+							onChange={(event) => setIsVaccined(!isVaccined)}
+							inputProps={{ "aria-label": "controlled" }}
+							value="remember"
+							color="primary"
+						/>
+					}
+					label="¿Vacunado?"
+				/>
+			</div>
+			{isVaccined && (
+				<div>
+					<TextField
+						id="outlined-select-currency"
+						select
+						label="Select"
+						value={vaccineType}
+						onChange={(event) => setVaccineType(event.target.value)}
+						helperText="Selecciona tipo de vacuna"
+					>
+						{vaccines.map((vaccine) => (
+							<MenuItem key={vaccine.id} value={vaccine.name}>
+								{vaccine.name}
+							</MenuItem>
+						))}
+					</TextField>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="vaccinated_date"
+						label="Fecha de vacunación"
+						name="vaccinated_date"
+						type="date"
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="vaccinated_dose"
+						label="Número de dosis"
+						name="vaccinated_dose"
+						type="number"
+					/>
+				</div>
+			)}
+			<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+				Actualizar mis datos
+			</Button>
+		</Box>
+	);
 };
 
 export default FormEmployee;
--- kruger_frontend/src/components/HomeEmployee.jsx
+++ kruger_frontend/src/components/HomeEmployee.jsx
@@ -1,12 +1,19 @@
+import { Box, Container, createTheme, CssBaseline, Grid, ThemeProvider, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
+import FormEmployee from "./FormEmployee";
 
 const cookies = new Cookies();
+const theme = createTheme();
 
 const HomeEmployee = () => {
 	const [employee, setEmployee] = useState(null);
 	const [loadPage, setLoadPage] = useState(false);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
 
 	useEffect(() => {
 		const fetchEmployee = () => {
@@ -28,7 +35,29 @@ const HomeEmployee = () => {
 
 	if (loadPage) return <div>Cargando...</div>;
 
-	return <div>Bienvenido {employee?.name}</div>;
+	return (
+		<Container maxWidth="lg">
+			<main>
+				<Grid container spacing={2}>
+					<Grid item xs={6}>
+						<Typography>Nombres: {employee?.name}</Typography>
+						<Typography>Apellidos: {employee?.last_name}</Typography>
+						<Typography>Cédula: {employee?.ci}</Typography>
+						<Typography>Correo: {employee?.email}</Typography>
+					</Grid>
+					{employee?.vaccinated && (
+						<Grid item xs={6}>
+							<Typography>Vacunado: {employee?.vaccinated}</Typography>
+							<Typography>Tipo de vacuna: {employee?.vaccinated_type}</Typography>
+							<Typography>Fecha vacunación: {employee?.vaccinated_date}</Typography>
+							<Typography>Número de dosis de la vacuna: {employee?.vaccinated_dose}</Typography>
+						</Grid>
+					)}
+				</Grid>
+				<FormEmployee employee={employee} />
+			</main>
+		</Container>
+	);
 };
 
 export default HomeEmployee;
