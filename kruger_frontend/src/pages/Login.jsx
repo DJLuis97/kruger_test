9cb496f42e1763faf91924079f40e2cc35975cae
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-29 06:15:49 -0500

    Se agrego plantilla para formulario de login

--- kruger_frontend/src/pages/Login.jsx
+++ kruger_frontend/src/pages/Login.jsx
@@ -1,3 +1,70 @@
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Container } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
 export default function LoginPage() {
-	return <div>Login Page</div>;
+	const theme = createTheme();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+
+		console.log({
+			email: data.get("email"),
+			password: data.get("password")
+		});
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center"
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Login
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="username"
+							label="Nombre de usuario"
+							name="username"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Contraseña"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+							Iniciar sesión
+						</Button>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 }
