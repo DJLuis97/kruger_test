db42e53be44903884038ad890373dee9868195de
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-29 03:21:19 -0500

    Se instalo react router
    ejecutamos el comando `npm i react-router-dom`

---
+++ kruger_frontend/src/Router.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+function Router() {
+	return <BrowserRouter></BrowserRouter>;
+}
+
+export default Router;
